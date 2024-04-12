1943514c9bf70bd5a23c9b3a7a2f85cd421cdd20
gautam-khaire <gautam.khaire@stride.ai>
2024-04-12 12:34:12 +0530

    Undo & Redo feature completed

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,19 @@ import { useEffect, useRef, useState } from "react";
 import { fabric } from "fabric";
 import { handleCanvasMouseDown, handleCanvasMouseUp, initializeFabric,handleResize, handleCanvasMouseMove, renderCanvas, handleCanvasObjectModified } from "@/lib/canvas";
 import { ActiveElement } from "@/types/type";
-import { useMutation, useStorage } from "@/liveblocks.config";
+import { useMutation, useRedo, useStorage, useUndo } from "@/liveblocks.config";
 import { defaultNavElement } from "@/constants";
 import { handleDelete, handleKeyDown } from "@/lib/key-events";
 
 export default function Page() {
+  const undo = useUndo();
+  const redo = useRedo();
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const fabricRef = useRef<fabric.Canvas | null>(null);
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
-  const selectedShapeRef = useRef<string | null>('rectangle');
+  const selectedShapeRef = useRef<string | null>(null);
   const activeObjectRef = useRef<fabric.Object | null>(null);
 
   const canvasObjects = useStorage((root)=>root.canvasObjects)
@@ -124,6 +126,17 @@ export default function Page() {
     window.addEventListener("resize", () => {
       handleResize({ fabricRef });
     });
+
+    window.addEventListener("keydown",(e: any)=>{
+      handleKeyDown({
+        e,
+        canvas: fabricRef.current,
+        undo,
+        redo,
+        syncShapeInStorage,
+        deleteShapeFromStorage
+      })
+    })
 
     return () => {
       canvas.dispose();
